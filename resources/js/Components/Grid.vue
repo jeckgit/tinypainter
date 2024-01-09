42165214472dd6ee2d1804aae1b6d384f26d243b
Eckhold Jörg <eckholdjorg@Eckholds-Laptop.fritz.box>
2024-01-09 23:04:07 +0100

    [+] Init

---
+++ resources/js/Components/Grid.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const CELL_SIZE = 15;
+
+const windowHeight = ref(window.innerHeight);
+const windowWidth = ref(window.innerWidth);
+
+const vertivalNumSquares = Math.floor(windowHeight.value / CELL_SIZE);
+const horizontalNumSquares = Math.floor(windowWidth.value / CELL_SIZE);
+
+const getRandomHexColor = () => {
+  // Generate random values for red, green, and blue components
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  // Convert the decimal values to hexadecimal and concatenate
+  const hexColor = `#${(red).toString(16).padStart(2, '0')}${(green).toString(16).padStart(2, '0')}${(blue).toString(16).padStart(2, '0')}`;
+
+  return hexColor;
+}
+
+</script>
+
+<template>
+  <div class="fixed">
+    <p>height: {{ windowHeight }}</p>
+    <p>width: {{ windowWidth }}</p>
+    <p>num squares vert: {{ vertivalNumSquares }} </p>
+    <p>num squares hor: {{ horizontalNumSquares }} </p>
+  </div>
+  <div class="absolute top-0 left-0">
+    <div v-for="(vert, idx) in vertivalNumSquares" :key="`vert-${idx}`" class="vert flex">
+      <div v-for="(hor, idx) in horizontalNumSquares" :key="`hor-${idx}`" class="hor cell"
+        :style="{ height: CELL_SIZE + 'px', width: CELL_SIZE + 'px', backgroundColor: getRandomHexColor() }" />
+    </div>
+  </div>
+</template>
+<style lang="css">
+.vert {
+  display: flex;
+  width: 100%;
+}
+.cell {}
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+import Grid from '@/Components/Grid.vue' 
+
+defineProps<{
+  canLogin?: boolean;
+  canRegister?: boolean;
+  laravelVersion: string;
+  phpVersion: string;
+}>();
+</script>
+
+<template>
+  <Head title="Welcome" />
+
+  <div
+    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center selection:bg-red-500 selection:text-white">
+    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
+      <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+      Dashboard</Link>
+
+      <template v-else>
+        <Link :href="route('login')"
+          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+        Log in</Link>
+
+        <Link v-if="canRegister" :href="route('register')"
+          class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+        Register</Link>
+      </template>
+    </div>
+    <grid />
+
+  </div>
+</template>
+
+<style></style>
